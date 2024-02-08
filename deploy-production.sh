1abea0f8a607e192cddc2ef9e04bd31b58833a23
lance <lance@freelancelance.com>
2024-02-08 15:57:20 -0500

    fixed bug where migrations weren't happening

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/django-projects/api_bddw_com
 git pull origin production
 sudo docker compose -f production.yml down
-sudo docker compose -f production.yml run --rm django python manage.py migrate
 sudo docker compose -f production.yml up --build -d
+sudo docker compose -f production.yml run --rm django python manage.py migrate
